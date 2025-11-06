7cf5e64f611783722c7272e85c454ce57de90980
Buddhi Perera <ghp7482@nyu.edu>
2025-11-06 11:18:46 -0500

    added decryption shift rows

--- aes/rtl/shift_rows.sv
+++ aes/rtl/shift_rows.sv
@@ -1,8 +1,10 @@
-module shift_rows (
+module shift_rows #(
+    parameter logic INVERSE = 1'b0
+)(
     input  logic [127:0] input_state,
     output logic [127:0] output_state
 );
-    // TODO: 
+    // TODO: need register when pipelining
 
     // Byte arrays (column-major: index = 4*col + row)
     logic [7:0] s [0:15];
@@ -16,15 +18,31 @@ module shift_rows (
         end
     endgenerate
 
-    // ShiftRows mapping (encryption)
-    // Row 0
-    assign t[0]  = s[0];   assign t[4]  = s[4];   assign t[8]  = s[8];   assign t[12] = s[12];
-    // Row 1
-    assign t[1]  = s[5];   assign t[5]  = s[9];   assign t[9]  = s[13];  assign t[13] = s[1];
-    // Row 2
-    assign t[2]  = s[10];  assign t[6]  = s[14];  assign t[10] = s[2];   assign t[14] = s[6];
-    // Row 3
-    assign t[3]  = s[15];  assign t[7]  = s[3];   assign t[11] = s[7];   assign t[15] = s[11];
+    generate
+        if (!INVERSE) begin : Encryption
+            // ShiftRows mapping: left shift(encryption)
+            // Row 0
+            assign t[0]  = s[0];   assign t[4]  = s[4];   assign t[8]  = s[8];   assign t[12] = s[12];
+            // Row 1
+            assign t[1]  = s[5];   assign t[5]  = s[9];   assign t[9]  = s[13];  assign t[13] = s[1];
+            // Row 2
+            assign t[2]  = s[10];  assign t[6]  = s[14];  assign t[10] = s[2];   assign t[14] = s[6];
+            // Row 3
+            assign t[3]  = s[15];  assign t[7]  = s[3];   assign t[11] = s[7];   assign t[15] = s[11];
+        end
+        else begin : Decryption
+            // ShiftRows mapping:right shift (decryption)
+            // Row 0
+            assign t[0]  = s[0];    assign t[4]  = s[4];     assign t[8]  = s[8];   assign t[12] = s[12];
+            // Row 1
+            assign t[1]  = s[13];     assign t[5]  = s[1];    assign t[9]  = s[5];    assign t[13] = s[9];
+            // Row 2
+            assign t[2]  = s[10];     assign t[6]  = s[14];     assign t[10] = s[2];    assign t[14] = s[6];
+            // Row 3
+            assign t[3]  = s[7];     assign t[7]  = s[11];     assign t[11] = s[15];     assign t[15] = s[3];
+        end
+    endgenerate
+    
 
     // Re-pack bytes to output_state
     generate
--- aes/tb/tb_shift_rows.sv
+++ aes/tb/tb_shift_rows.sv
@@ -1,11 +1,15 @@
 module tb_shift_rows ();
 
-    logic [127:0] input_state;
-    logic [127:0] output_state;
+    logic [127:0] input_state_encrypt;
+    logic [127:0] output_state_encrypt;
 
-    shift_rows dut(.*);
+    logic [127:0] input_state_decrypt;
+    logic [127:0] output_state_decrypt;
 
-    task check_output(logic [127:0] expected_output_state);
+    shift_rows #(.INVERSE(0)) dut_encrypt (.input_state(input_state_encrypt), .output_state(output_state_encrypt));
+    shift_rows #(.INVERSE(1)) dut_decrypt (.input_state(input_state_decrypt), .output_state(output_state_decrypt));
+
+    task check_output(logic [127:0] output_state, logic [127:0] expected_output_state);
         if (output_state !== expected_output_state) begin
             $display("FAILED: Output state missmatch!");
             $display("Expected: %h\nGot: %h", expected_output_state, output_state);
@@ -14,9 +18,13 @@ module tb_shift_rows ();
     endtask
 
     initial begin
-        input_state = 128'h89c2abb23688ac1c675eb2d4cf2a263e;
+        input_state_encrypt = 128'h89c2abb23688ac1c675eb2d4cf2a263e;
         #1;
-        check_output(128'h365e26b2672aab1ccfc2acd48988b23e);
+        check_output(output_state_encrypt , 128'h365e26b2672aab1ccfc2acd48988b23e);
+
+        input_state_decrypt = 128'h365e26b2672aab1ccfc2acd48988b23e;
+        #1;
+        check_output(output_state_decrypt , 128'h89c2abb23688ac1c675eb2d4cf2a263e);
     end
 
     initial begin

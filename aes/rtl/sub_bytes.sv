07ef9a896550169b5fa3bce5cd33d701284ff860
milan344 <95296263+milan344@users.noreply.github.com>
2025-11-09 12:17:50 -0500

    Refactor sub_bytes module to make it combinational
    Removed clock and enable signals, simplifying the module.

--- aes/rtl/sub_bytes.sv
+++ aes/rtl/sub_bytes.sv
@@ -1,38 +1,22 @@
 module sub_bytes (
-	input  logic        clk,
-	input  logic        enable,
-	input  logic [127:0] in,
-	output logic [127:0] out,
-	output logic sub_ready
+  input  logic [127:0] in,
+  output logic [127:0] out
 );
-timeunit 1ns/1ps;
-	logic [127:0] out_comb; 
 
-	genvar i;
-	generate
-		for (i = 0; i < 16; i++) begin : gen_sbox
-			s_box u_s_box (
-				.in (in [8*i +: 8]),
-				.out(out_comb[8*i +: 8])
-			);
-		end
-	endgenerate
+  timeunit 1ns/1ps;
 
-    
-    always_ff @(posedge clk) begin
-		if (enable) begin
-			out <= out_comb;
-			sub_ready <= 1'b1;
-		end
-		else begin
-			out <= 128'd0;
-			sub_ready <= 1'b0;
-		end
+  // 16 parallel S-boxes, each processing one byte
 
+  genvar i;
+  generate
+    for (i = 0; i < 16; i++) begin : gen_sbox
+      s_box u_s_box (
+        .in  ( in [8*i +: 8] ),
+        .out ( out[8*i +: 8] )
+      );
     end
-
+  endgenerate
+	
 endmodule
-
-
 
 

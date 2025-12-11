9b6f4ad9edfc4783e6589b1493f9ea9829ef4dd3
Buddhi Perera <ghp7482@nyu.edu>
2025-12-11 18:50:41 -0500

    moved the trl files

--- aes/rtl/add_roundkey.sv
+++
@@ -1,9 +0,0 @@
-module add_roundkey (
-    input logic [127:0] input_state,
-    input logic [127:0] key,
-    output logic [127:0] output_state
-);
-
-assign output_state = input_state ^ key;
-    
-endmodule

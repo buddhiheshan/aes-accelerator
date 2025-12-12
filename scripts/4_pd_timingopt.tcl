15ce9a8e95838e990931a212affda84618166374
jy5187 <jy5187@ecs03.poly.edu>
2025-12-11 20:00:24 -0500

    updates to scripts

--- scripts/4_pd_timingopt.tcl
+++ scripts/4_pd_timingopt.tcl
@@ -1,7 +1,6 @@
 set DATE [clock format [clock seconds] -format "%b%d"] 
 set REPORT_DIR "physical_design/logs/report_${DATE}"
 
-time_design -pre_cts -num_paths 50 -report_prefix pre_cts 
-opt_design -pre_cts -drv -report_dir $OUTPUT_DIR
-opt_design -pre_cts -hold -drv -report_dir $OUTPUT_DIR
-opt_design -pre_cts -hold -setup -drv -report_dir $OUTPUT_DIR
+#time_design -pre_cts -num_paths 50 -report_prefix pre_cts 
+opt_design -pre_cts -drv -report_dir $REPORT_DIR
+
